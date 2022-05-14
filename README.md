d47b229417820b212e435f548d08e941a0862916
kiririmode <kiririmode@gmail.com>
2022-05-14 18:35:12 +0900

    add: TODO

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # sandbox-cognito
 
 - [ ] Hosted UIに対して独自ドメインを適用する
-  - [ ] Hosted UI は CloudFront でサーブされるため、証明書は us-east-1 で取得する
+  - [x] Hosted UI は CloudFront でサーブされるため、証明書は us-east-1 で取得する
+  - [ ] Hosted UI を画面表示する
---
+++ aws/variables.tf
@@ -0,0 +1,25 @@
+variable "custom_domain" {
+  type        = string
+  description = "独自ドメイン名。最後のピリオドは不要 (ex., example.com)"
+}
+
+variable "hostedui_domain_prefix" {
+  type        = string
+  description = "Cognito の Hosted UI に付与するドメインのプレフィックス"
+}
+
+variable "create_dummy_record" {
+  type        = bool
+  description = <<EOT
+`custom_domain` の配下にダミーの A レコードを作る場合に true を指定する。
+
+Cognito の Hosted UI にカスタムドメインを割り当てようとする場合、Cognito は
+ルートドメインに対する A レコードを要求するため。
+
+ref: 
+
+- https://stackoverflow.com/questions/51249583/cognito-own-domain-name-required-a-record
+- https://qiita.com/afukuma/items/4074fda7fa158eb387a3
+EOT
+  default     = false
+}
